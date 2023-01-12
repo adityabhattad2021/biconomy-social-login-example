cfdd06d75ba20b171bdb4e2939b5a4a0ae8a84ed
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-12 22:34:17 +0530

    Initial Setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  webpack:(config,{isServer})=>{
+    if(!isServer){
+      config.resolve.fallback={
+        "fs":false,
+        "net":false,
+        "tls":false
+      }
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
