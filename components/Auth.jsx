6ba0d93ef088c4e3ca683cb08bb4651ea8d6157e
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-12 22:54:17 +0530

    Add Login Function

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -1,10 +1,40 @@
-function Auth(){
-    return (
-        <div>
-            Auth
-        </div>
-    )
-}
+import { useState, useEffect, useRef } from "react";
+import SocialLogin, { socialLoginSDK } from "@biconomy/web3-auth";
+import { ChainId } from "@biconomy/core-types";
+import SmartAccount from "@biconomy/smart-account";
+import { ethers } from "ethers";
+import { css } from "@emotion/css";
 
+
+function Auth() {
+
+    const [smartAccount,setSmartAccount]=useState(null);
+    const [interval,enableInterval]=useState(false);
+    const sdkRef=useRef(null);
+    const [loading,isLoading]=useState(false);
+
+
+    async function login(){
+        if(!sdkRef.current){
+            const SocialLoginSDK=new SocialLogin()
+            await socialLoginSDK.init(ethers.utils.hexValue(ChainId.POLYGON_MUMBAI))
+            sdkRef.current=socialLoginSDK;
+        }
+        if(!sdkRef.current.provider){
+            sdkRef.current.showConnectModal()
+            sdkRef.current.showWallet()
+            enableInterval(true)
+        }else{
+            setupSmartAccount()
+        }
+    }
+
+    async function setupSmartAccount(){
+        
+    }
+
+
+	return <div>Auth</div>;
+}
 
 export default Auth;
