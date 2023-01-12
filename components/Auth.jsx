c87a681b623c1b5406c3ecb422c56f54f0bc9de4
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-12 23:04:02 +0530

    Add setupSmartAccount functionality

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -5,34 +5,61 @@ import SmartAccount from "@biconomy/smart-account";
 import { ethers } from "ethers";
 import { css } from "@emotion/css";
 
-
 function Auth() {
+	const [smartAccount, setSmartAccount] = useState(null);
+	const [interval, enableInterval] = useState(false);
+	const sdkRef = useRef(null);
+	const [isloading, setIsLoading] = useState(false);
 
-    const [smartAccount,setSmartAccount]=useState(null);
-    const [interval,enableInterval]=useState(false);
-    const sdkRef=useRef(null);
-    const [loading,isLoading]=useState(false);
+	useEffect(() => {
+		let configureLogin;
+		if (interval) {
+			configureLogin = setInterval(() => {
+				if (!!sdkRef.current?.provider) {
+					setupSmartAccount();
+					clearInterval(configureLogin);
+				}
+			}, 1000);
+		}
+	}, [interval]);
 
+	async function login() {
+		if (!sdkRef.current) {
+			const SocialLoginSDK = new SocialLogin();
+			await socialLoginSDK.init(
+				ethers.utils.hexValue(ChainId.POLYGON_MUMBAI)
+			);
+			sdkRef.current = socialLoginSDK;
+		}
+		if (!sdkRef.current.provider) {
+			sdkRef.current.showConnectModal();
+			sdkRef.current.showWallet();
+			enableInterval(true);
+		} else {
+			setupSmartAccount();
+		}
+	}
 
-    async function login(){
-        if(!sdkRef.current){
-            const SocialLoginSDK=new SocialLogin()
-            await socialLoginSDK.init(ethers.utils.hexValue(ChainId.POLYGON_MUMBAI))
-            sdkRef.current=socialLoginSDK;
-        }
-        if(!sdkRef.current.provider){
-            sdkRef.current.showConnectModal()
-            sdkRef.current.showWallet()
-            enableInterval(true)
-        }else{
-            setupSmartAccount()
-        }
-    }
+	async function setupSmartAccount() {
+		setIsLoading(true);
+		sdkRef.current.hideWallet();
 
-    async function setupSmartAccount(){
-        
-    }
+		const web3Provider = new ethers.providers.Web3Provider(
+			sdkRef.current.provider
+		);
 
+		try {
+			const smartAccount = new SmartAccount(web3Provider, {
+				activeNetworkId: ChainId.POLYGON_MUMBAI,
+				supportedNetworksIds: [ChainId.GOERLI, ChainId.POLYGON_MUMBAI],
+			});
+			await smartAccount.init();
+			setSmartAccount(smartAccount);
+			setIsLoading(false);
+		} catch (error) {
+			console.log("Error setting up a smart contract: ", error);
+		}
+	}
 
 	return <div>Auth</div>;
 }
